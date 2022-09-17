d33caa3af62d56c4c9092817b3ac16a11005ec74
RafaSolorio <rafavsgc@gmail.com>
2022-09-17 14:14:01 -0700

    table of contents created

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var links = [
+    {
+        label: "Week 1 notes",
+        url: "week1/index.html"
+    }
+]
+
+links.forEach(link => {
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+    a.setAttribute('href', link.url);
+    a.textContent = link.label;
+    li.appendChild(a);
+    document.getElementById('list').appendChild(li);
+})
