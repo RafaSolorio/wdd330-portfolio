3e4365e4a4c648160bea2fe86570478882dfe206
RafaSolorio <rafavsgc@gmail.com>
2022-09-24 22:23:22 -0700

    Added notes

--- quiz/main.js
+++ quiz/main.js
@@ -4,24 +4,35 @@ const quiz = [
     ["What is Batman's real name?","Bruce Wayne"]
 ];
 
-let score = 0 // initialize score
+function start(quiz){
+    alert('Welcome to Quiz Ninja!');
+    let score = 0;
 
-alert('Welcome to Quiz Ninja!');
+    // main game loop
+    for(const [question,answer] of quiz){
+        const response = ask(question);
+        check(response,answer);
+    }
+    // end of main game loop
 
-for(const [question,answer] of quiz){
-    const response = prompt(question);
-    if(response === answer){
+    gameOver();
+
+    // function declarations
+    function ask(question){
+        return prompt(question);
+    }
+
+    function check(response,answer){
+        if(response === answer){
         alert('Correct!');
         score++;
-    } else {
+        } else {
         alert(`Wrong! The correct answer was ${answer}`);
+        }
+    }
+
+    function gameOver(){
+        alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
     }
 }
-
-// At the end of the game, report the player's score
-alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
-
-//alert('Welcome to Quiz Ninja!');
-//const question = "What is Superman's real name?"
-//const answer = prompt(question);
-//alert(`You answered ${answer}`);
+start(quiz);
