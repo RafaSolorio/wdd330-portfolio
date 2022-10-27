127e6c5f2a26e80cf2e79454eb93f075dad99769
Rafael Solorio <rafavsgc@gmail.com>
2022-10-27 16:29:34 -0700

    working with one file

--- todo_app/ToDos.js
+++ todo_app/ToDos.js
@@ -1,33 +1,32 @@
 const ToDosList = [];
   
-  export default class ToDos {
-    constructor(elementId) {
-      this.parentElement = document.getElementById(elementId);
-    }
-    getAllToDos() {
-      return ToDosList;
-    }
-
-    showToDosList() {
-      this.parentElement.innerHTML = '';
-
-      renderToDosList(this.parentElement, this.getAllToDos());
-    }
-    
-    addTask(task){
-        ToDosList.push(task)
-    }
+export default class ToDos {
+  constructor(elementId) {
+    this.parentElement = document.getElementById(elementId);
+  }
+  getAllToDos() {
+    return ToDosList;
+  }
+  showToDosList() {
+    //this.parentElement.innerHTML = '';
+    renderToDosList(this.parentElement, this.getAllToDos());
+  }
+  
+  addTask(task){
+      ToDosList.push(task)
   }
 
-  function renderToDosList(parent, todos) {
-    todos.forEach(task => {
-      parent.appendChild(renderOneTask(task));
-    });
-  }
-  function renderOneTask(task) {
-    const item = document.createElement('li');
-    item.innerHTML = `<input type="checkbox" name="completed">
-        <p>${task.content}</p  
-        `;
-    return item;
-  }
+}
+function renderToDosList(parent, todos) {
+  todos.forEach(task => {
+    parent.appendChild(renderOneTask(task));
+  });
+}
+function renderOneTask(task) {
+  const item = document.createElement('li');
+  item.innerHTML = task.content;
+  console.log(item)
+  return item;
+}
+
+console.log(ToDosList);
--- todo_app/main.js
+++ todo_app/main.js
@@ -1,19 +1,45 @@
-import ToDos from "./ToDos.js";
-
-const todos = new ToDos("todoList");
-
-const form = document.forms['task'];
-form.addEventListener('submit', createTask, false);
+function renderTask(task) {
+  const todoList = document.querySelector('#todoList');
 
 
-window.addEventListener("load", () => {
-  todos.showToDosList();
+  const isChecked = task.checked ? 'done': '';
+  const node = document.createElement("li");
+  node.setAttribute('class', `todo-item ${isChecked}`);
+  node.setAttribute('data-key', task.id);
+  node.innerHTML = `
+    <input id="${task.id}" type="checkbox"/>
+    <label for="${task.id}" class="tick"></label>
+    <span>${task.text}</span>
+    <button class="delete-task">X</button>
+  `;
+
+  todoList.append(node);
+  console.log(todoItems);
+}
+
+let ToDos = [];
+
+function addTask(text) {
+  const task = {
+    id: Date.now(),
+    text,
+    completed: false,
+  };
+
+  ToDos.push(task);
+  renderTask(task);
+}
+
+const form = document.querySelector('#task');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const input = document.querySelector('#task-input');
+
+  const text = input.value.trim();
+  if (text !== '') {
+    addTask(text);
+    input.value = '';
+    input.focus();
+  }
 });
 
-function createTask(event) {
-    event.preventDefault()
-    const task = {};
-    task.content = form.taskContent.value;
-    todos.addTask(task)
-    return task
-}
